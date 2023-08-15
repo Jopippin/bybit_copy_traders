a3f9a80e7866a0176f7bc51dab19f84db906970e
Jopippin <135984511+Jopippin@users.noreply.github.com>
2023-08-15 22:16:06 +0200

    nc

--- tw2.py
+++ tw2.py
@@ -135,7 +135,7 @@ while page_no <= total_pages:
                                 trade_identifier = f"{created_at}"
                                 trade_already_logged = False
 
-                                with open("trade_log.txt", "r", encoding="utf-8") as fp:
+                                with open("trade_log.txt", "r") as file:
                                     for line in file:
                                         if trade_identifier in line:
                                             trade_already_logged = True
@@ -147,9 +147,9 @@ while page_no <= total_pages:
 
                         if formatted_trades:
                             # Log trades to the CSV file
-                            with open("trade_log.txt", "a", encoding="utf-8") as fp:
+                            with open("trade_log.txt", "a") as file:
                                 for trade in formatted_trades:
-                                    outfile.write(trade + "\n")
+                                    file.write(trade + "\n")
 
                             webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1140007491850211459/8gGy_GBT0LwgDXMrsJxnG15GqZ7p7PtJHV5VHYxDLq-QDxCJquapO0bQL5Y11akxhnzV')
                             embed = DiscordEmbed(title=f"{nick_name} Opened Some Trades", color=242424)
