e10b579573ddcdf4e485ba215317bf22183f494c
Jopippin <135984511+Jopippin@users.noreply.github.com>
2023-08-17 14:06:59 +0200

    nc

--- tw2.py
+++ tw2.py
@@ -136,7 +136,7 @@ while page_no <= total_pages:
                                 trade_identifier = f"{created_at}"
                                 trade_already_logged = False
 
-                                with open("/home/runner/work/bybit_copy_traders/bybit_copy_traders/trade_log.csv", "r", encoding="utf-8") as file:
+                                with open("/home/runner/work/bybit_copy_traders/trade_log.csv", "r", encoding="utf-8") as file:
                                     for line in file:
                                         if trade_identifier in line:
                                             trade_already_logged = True
@@ -147,7 +147,7 @@ while page_no <= total_pages:
                                     logged_trade_ids.add(created_at)
 
                         if formatted_trades:
-                            with open("/home/runner/work/bybit_copy_traders/bybit_copy_traders/trade_log.csv", "a", encoding="utf-8") as file:
+                            with open("/home/runner/work/bybit_copy_traders/trade_log.csv", "a", encoding="utf-8") as file:
                                 for trade in formatted_trades:
                                     file.write(trade + "\n")
 
